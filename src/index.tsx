eb83a4eaa2acc22cd1aa24cad7f53c9aade186fb
yukiaoki2024-prog <yukiaoki.2024@gmail.com>
2026-06-10 12:31:06 +0900

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // もしグローバルなCSSがあれば
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Failed to find the root element");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
